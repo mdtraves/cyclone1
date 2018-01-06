56714782f040d0ff22362b3b6deacb12348ac509
mike <mdtraves@hotmail.com>
2018-01-06 19:27:06 +0000

    added gulp watch functionality

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+let gulp = require("gulp");
+let sass = require("gulp-sass");
+let clean = require("gulp-clean-css");
+let runSequence = require('gulp-run-sequence');
+
+gulp.task("sass", function(){
+    return gulp.src("app/scss/main.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+});
+
+gulp.task("clean", function(){
+    return gulp.src("app/css/*.css")
+        .pipe(clean())
+        .pipe(gulp.dest("dist/css"))
+});
+
+
+gulp.task('dev', function(done) {
+    runSequence('sass', 'clean', function() {
+        console.log('Run something else');
+        done();
+    });
+});
+
+gulp.task ("watch", () => {
+    gulp.watch("app/scss/main.scss", ["dev"]);
+})
---
+++ header.php
@@ -0,0 +1,36 @@
+<header class="header">
+    <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse ">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container">
+        <a class="navbar-brand" href="#"><img src="app/img/cyclonetranswhite.png" alt="brand logo"></a>
+
+        <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ABOUT</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">GALLERY</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">LOCATIONS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">CONTACT</a>
+                </li>
+            </ul>
+        </div>
+        <div class="nav-info">
+            <i class="fa fa-phone" aria-hidden="true"></i>
+            <p class="contactNumber">07795158845</p>
+            <a href="https://www.facebook.com/cyclonekickboxing/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <p>CYCLONE</p>
+        </div>
+        </div>
+    </nav>
+</header>
