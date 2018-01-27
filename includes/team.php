c256edce6c2f169f26cbaa866d96ec491dffed5c
mike <mdtraves@hotmail.com>
2018-01-27 21:57:23 +0000

    added more staff to the team page

--- includes/team.php
+++ includes/team.php
@@ -15,6 +15,34 @@
               </div>
           </div>
       </div>
+      <div class="row">
+          <div class="col-sm-6 text-center">
+              <div class="team-box">
+                  <img src="dist/img/ellieHarper.png" class="" alt="Ellie">
+                  <h3>Ellie Harper</h3>
+              </div>
+          </div>
+          <div class="col-sm-6 text-center">
+              <div class="team-box">
+                  <img src="dist/img/miaMaitland.png" class="" alt="">
+                  <h3>Mia Maitland</h3>
+              </div>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-sm-6 text-center">
+              <div class="team-box">
+                  <img src="dist/img/ibrahimIbdali.png" class="" alt="david">
+                  <h3>Ibrahim Ibdali</h3>
+              </div>
+          </div>
+          <div class="col-sm-6 text-center">
+              <div class="team-box">
+                  <img src="dist/img/nickWheelhouse.png" class="" alt="">
+                  <h3>Nick Wheelhouse</h3>
+              </div>
+          </div>
+      </div>
   </div>
 </section>
 
